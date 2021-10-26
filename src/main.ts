fcda123b80528474d07c41cd7d105331a50992cd
TimL135 <t.littschwager@mbd-team.de>
2021-10-26 21:32:13 +0200

    pwa finally

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import './registerServiceWorker';
+import "./registerServiceWorker";
 
 createApp(App).use(router).mount("#app");
