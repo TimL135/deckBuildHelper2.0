41aba6d49b28c3e4ca5b3d0d790f0d79e01c8c62
TimL135 <t.littschwager@mbd-team.de>
2021-10-26 23:04:08 +0200

    fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,24 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.755c86ddb6f6be2698453a9b55645b2a.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "deckbuildhelper2.0"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+ importScripts(
+   "/precache-manifest.755c86ddb6f6be2698453a9b55645b2a.js"
+ );
+ 
+ workbox.core.setCacheNameDetails({prefix: "deckbuildhelper2.0"});
+ 
+ self.addEventListener('message', (event) => {
+   if (event.data && event.data.type === 'SKIP_WAITING') {
+     self.skipWaiting();
+   }
+ });
+ 
+ /**
+  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+  * requests for URLs in the manifest.
+  * See https://goo.gl/S9QRab
+  */
+ self.__precacheManifest = [].concat(self.__precacheManifest || []);
+ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
