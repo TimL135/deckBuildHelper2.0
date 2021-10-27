98df3e6d82898818ddefc16438842a9b4a6200bf
TimL135 <t.littschwager@mbd-team.de>
2021-10-27 17:48:40 +0200

    rename

---
+++ docs/precache-manifest.755c86ddb6f6be2698453a9b55645b2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d235fa0fcac361024e",
+    "url": "/css/app.bbc5f530.css"
+  },
+  {
+    "revision": "693965172016cd97e8173e2c91d5def2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "245d53b50f94c23015db",
+    "url": "/js/about.5f20c787.js"
+  },
+  {
+    "revision": "82d235fa0fcac361024e",
+    "url": "/js/app.b62d012e.js"
+  },
+  {
+    "revision": "23d6af565c83922c8011",
+    "url": "/js/chunk-vendors.9f9e81a2.js"
+  },
+  {
+    "revision": "0328b1f49adee7f877ec6161288a62f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
