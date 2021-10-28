1e06d2dae78114d3fb38865300e59b31a3e25129
tim <t.littschwager@mbd-team.de>
2021-10-28 09:59:45 +0200

    vue add pwa

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: "/deckBuildHelper2.0/",
+  outputDir: "./docs",
+  //pwa: {
+  //workboxPluginMode: "InjectManifest",
+  //workboxOptions: {
+  //swSrc: "src/service-worker.js",
+  //},
+  //},
+};
