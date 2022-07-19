30efd156b235a3f9ab74df3c894ac97a39e3eab0
TimL135 <t.littschwager@mbd-team.de>
2022-07-19 14:43:19 +0200

    test

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,13 @@
 module.exports = {
   publicPath: "/deckBuildHelper2.0/",
   outputDir: "./docs",
-  pwa: {
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      swSrc: "src/registerServiceWorker.ts"
-    },
-    name: "Test",
-    appleMobileWebAppCapable: true,
-    themeColor: "black",
-    msTileColor: "black",
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    manifestOptions: {
-      background_color: "black"
-      // display: 'landscape',
-    }
-  }
+  //pwa: {
+  //workboxPluginMode: "InjectManifest",
+  //workboxOptions: {
+  //swSrc: "src/service-worker.js",
+  //},
+  //},
+  css: {
+    extract: false,
+  },
 };
