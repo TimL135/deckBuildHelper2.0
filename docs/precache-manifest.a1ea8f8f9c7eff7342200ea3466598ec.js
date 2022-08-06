ad039c225bd995e61c771c3bb7c0069161699d65
TimL135 <t.littschwager@mbd-team.de>
2022-08-06 21:37:12 +0200

    .

---
+++ docs/precache-manifest.a1ea8f8f9c7eff7342200ea3466598ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437b2ac868fdabded36aa4ae2526a3a6",
+    "url": "/deckBuildHelper2.0/img/cardBackside.437b2ac8.png"
+  },
+  {
+    "revision": "437b2ac868fdabded36aa4ae2526a3a6",
+    "url": "/deckBuildHelper2.0/img/cards/cardBackside.png"
+  },
+  {
+    "revision": "d9e3830409c706f9aee6a7a58c43400b",
+    "url": "/deckBuildHelper2.0/index.html"
+  },
+  {
+    "revision": "a86dd08e75f7f129f02f",
+    "url": "/deckBuildHelper2.0/js/app.11b117fb.js"
+  },
+  {
+    "revision": "3064b502548126c1cb8b",
+    "url": "/deckBuildHelper2.0/js/chunk-vendors.2dc734fe.js"
+  },
+  {
+    "revision": "0328b1f49adee7f877ec6161288a62f7",
+    "url": "/deckBuildHelper2.0/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/deckBuildHelper2.0/robots.txt"
+  }
+]);
