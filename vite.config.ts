e4d99811e0df47f304590dd33672da148c31b148
TimL135 <t.littschwager@mbd-team.de>
2022-08-09 18:35:56 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,38 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "./docs",
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
+      },
+      manifest: {
+        background_color: "#000000",
+        theme_color: "#7E1F86",
+        name: "deckBuilderHelper2.5",
+        short_name: "deckHelper2.5",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/public/img/cards/cardBackside.png",
+            sizes: "453x453",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
