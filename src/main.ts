a316371bd8d52d50398dbf23de3d2f073ea64c40
TimL135 <t.littschwager@mbd-team.de>
2022-08-10 18:42:29 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
-import "./registerServiceWorker";
+import "../sw.js";
 import "./main.scss";
 createApp(App).use(router).mount("#app");
