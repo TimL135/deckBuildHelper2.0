50f7e18bfbd5a255a61b2665942f656403165ac0
TimL135 <t.littschwager@mbd-team.de>
2022-08-10 19:08:56 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,17 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      injectRegister: "auto",
-      strategies: "generateSW",
-      srcDir: "src",
-      filename: "sw.js",
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg"],
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
-        swDest: "sw.js",
       },
       manifest: {
         background_color: "#000000",
         theme_color: "#7E1F86",
         name: "deckBuilderHelper2.5",
         short_name: "deckHelper2.5",
-        start_url: "/deckBuildHelper2.0",
+        start_url: "/deckBuildHelper2.0/",
         display: "standalone",
       },
     }),
@@ -37,5 +32,5 @@ export default defineConfig({
   server: {
     host: true,
   },
-  base: "./deckBuildHelper2.0",
+  base: "./deckBuildHelper2.0/",
 });
